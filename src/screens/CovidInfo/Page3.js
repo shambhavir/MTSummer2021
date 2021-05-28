75d43c8b12ea3a2d0dad5828d30b3f32552160e1
S <S@Shambhavis-MacBook.local>
2021-05-27 23:19:03 -0400

    added covid vaccine content

--- src/screens/CovidInfo/Page3.js
+++ src/screens/CovidInfo/Page3.js
@@ -432,7 +432,7 @@ export default class Page1 extends React.Component {
               fontStyle: "italic",
             }}
           >
-            Pregnant Women May Experience{"\n"}Different Symptoms If Infected
+            Should I be Concerned About Any Side Effects?
           </Text>
           <Text
             style={{
@@ -443,16 +443,193 @@ export default class Page1 extends React.Component {
               paddingHorizontal: 15,
             }}
           >
-            Studies have discovered that pregnant women may experience different
-            symptoms if infected with COVID-19. It is also important to mention
-            that this only applies to symptomatic pregnant women. A study found
-            that pregnant women did not report headaches, muscle aches, fever,
-            chills, and diarrhea nearly as much as non-pregnant women did.
-            However, both pregnant and non-pregnant women had similar rates of
-            coughing (~50%) and shortness of breath (30%). Thus, pregnant women
-            should remain cautious and get tested even if they don't display all
-            the symptoms common to COVID-19.
+            You can see a list of the possible side effects associated with each vaccine in the cards above. Clicking the link to each vaccine will bring you to a file with more information about each vaccine, provided by the FDA. Here are some important terms to know when discussing the COVID-19 vaccine.
           </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+            While symptoms may vary for each individual, it is important to consider the risks and rewards for your own body (for example, the risk of these vaccine symptoms compared to the reward of being protected from COVID-19). If you are concerned about side effects, be sure to consult your doctor before moving forward.
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 20,
+              color: "white",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+              fontWeight: "bold",
+              fontStyle: "italic",
+            }}
+          >
+            Should I Talk to My Doctor About Receiving the Vaccine? 
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           You should always consult your healthcare provider about major health decisions. For the COVID-19 Vaccine, be sure to talk to your doctor before receiving it if you: 
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           - Have allergies  
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           - Have a bleeding disorder or on a blood thinner   
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           - Are immunocompromised or take a medicine that affects your immune system   
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           - Are pregnant or plan to become pregnant
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+           - Are breastfeeding 
+
+          </Text>
+          
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 20,
+              color: "white",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+              fontWeight: "bold",
+              fontStyle: "italic",
+            }}
+          >
+            Do I Have to Pay to Receive the Vaccine?
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+          All individuals may receive the vaccine for free, regardless of whether or not they have insurance. Healthcare providers are not allowed to charge recipients or bill their balance. For individuals with Medicare Part B or Medicare Advantage Plan, there is no copayment, coinsurance or deductible for the COVID-19 vaccine. If you are covered by NJ FamilyCare (Medicaid or CHIP), the vaccine is also given without a copay. In other words, you will not have to pay any amount in order to get the COVID-19 vaccine. 
+
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 20,
+              color: "white",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+              fontWeight: "bold",
+              fontStyle: "italic",
+            }}
+          >
+            Is the Vaccine Actually Effective Against COVID-19? 
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+         Each type of vaccine has been proven to prevent recipients from getting COVID-19. However, the duration of protection, or how long this prevention lasts, is still unknown. Even if individuals are immune to the virus, they may still be carriers, or able to spread the illness to others. After receiving the vaccine, individuals must still wear a mask, wash their hands, and take other precautions against possibly infecting others with COVID-19.
+
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 20,
+              color: "white",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+              fontWeight: "bold",
+              fontStyle: "italic",
+            }}
+          >
+Current Thoughts on the Safety of the Vaccine for Pregnant Women
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+         From the CDC: “Based on how mRNA vaccines work, experts believe they are unlikely to pose a specific risk for people who are pregnant.” Even though there is no conclusive data currently available, scientists believe that the vaccine should be safe for pregnant women. Current COVID-19 vaccines are mRNA vaccines. These types of vaccines do not carry the live COVID-19 virus and expecting mothers cannot contract the virus or pass it to their child solely through getting vaccinated. 
+          </Text>
+          <Text
+            style={{
+              fontFamily: "System",
+              fontSize: 18,
+              color: "#F8F4FF",
+              paddingVertical: 8,
+              paddingHorizontal: 15,
+            }}
+          >
+        Our bodies break down the mRNA in the vaccine within hours/days, which means that it is unlikely that these particles will reach the placenta and affect the child in any way. Of course, these thoughts are simply speculation because no vaccine trials involving pregnant women have concluded yet. If you live in a setting where you are particularly susceptible to COVID, such as a group home, or are particularly at risk for complications, consider speaking to a health professional to weigh your options.
+          </Text>
+          <View style={{ justiftyContent: "center", alignItems: "center" }}>
           <Text
             style={{
               fontFamily: "System",
@@ -466,7 +643,6 @@ export default class Page1 extends React.Component {
           >
             Found this snippet useful? Favorite it!
           </Text>
-          <View style={{ justiftyContent: "center", alignItems: "center" }}>
             <AntDesign
               name="hearto"
               size={28}
