5fe7c766551c1afc10a3d9178d716d991dfae3a5
S <S@Shambhavis-MacBook.local>
2021-07-03 00:23:08 -0400

    stash

--- src/screens/LocationServices/LocationServices.js
+++ src/screens/LocationServices/LocationServices.js
@@ -31,25 +31,64 @@ import {
   SceneMap,
   TabView,
 } from "react-native-tab-view";
+import Expo from "expo"; 
 import MapView from 'react-native-maps';
+import { Location } from 'expo-location';
+//import * as Permissions from 'expo-permissions';
 
 const user = firebase.auth().currentUser;
 
 
 export default class LocationServices extends React.Component {
-  
+  constructor(props) {
+    super(props);
+    this.state =
+    {
+      mapRegion: null, 
+      location: {},
+      hasLocationPermissions: false, 
+      locationResult: null, 
+      errorMessage: ''
+    };
+  }
+  componentDidMount() {
+    this._getLocation(); 
+  }
+  // _handleMapRegionChange = mapRegion => {
+  //   console.log(mapRegion); 
+  //   this.setState({mapRegion}); 
+  // }; 
+
+  _getLocation = async () => {
+    const { status } = await Location.requestPermissionsAsync(); 
+    if (status != 'granted') {
+      //console.log('PERMISSION NOT GRANTED');
+      this.setState({
+        locationResult: 'PERMISSION NOT GRANTED'
+      }); 
+    }
+    const userLocation = await Location.getCurrentPositionAsync(); 
+    this.setState({
+      location: userLocation
+    })
+    console.log(userLocation)
+  }
+
   render() {
     return (
       <View
         style={styles.container}
       >
-        <MapView style={{flex:1}}
-  region={{latitude: 38.890316999999996,
-  longitude: -77.1542221,
-  latitudeDelta: 0.005,
-  longitudeDelta: 0.005}}/>
+        <Text>{JSON.stringify(this.state.location)}</Text>
+        <MapView style={{ flex: 1 }}
+          region={{
+            latitude: 38.890316999999996,
+            longitude: -77.1542221,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005
+          }} />
       </View>
-
+      
     );
   }
 }
--- src/screens/LocationServices/styles.js
+++ src/screens/LocationServices/styles.js
@@ -82,9 +82,5 @@ export default StyleSheet.create({
     fadingContainer: {
         padding: 20,
         backgroundColor: "powderblue"
-      },
-      map: {
-        width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
-      },
+      }
 })
